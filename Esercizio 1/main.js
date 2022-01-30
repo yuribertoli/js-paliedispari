0642b144f108fa7171c1d3f11d9b0c9c4a816621
yuribertoli <yuribertoli@yahoo.it>
2022-01-30 18:02:43 +0100

    frase palindroma

--- Esercizio 1/main.js
+++ Esercizio 1/main.js
@@ -1,5 +1,5 @@
 //Chiedo all'utente di inserire una parola 
-let domanda = prompt("Inserisci una parola");
+let domanda = prompt("Inserisci una parola o una frase");
 
 alert(parolaPalindroma(domanda));
 
@@ -8,24 +8,31 @@ function parolaPalindroma(parola) {
 
     let frase;
 
+    //Rendo tutto minuscolo per confrontare tutti i caratteri correttamente
+    let minuscolo = parola.toLowerCase();
+    console.log(minuscolo);
+
+    //Utilizzo .replace per rimuovere gli eventuali spazi tra le frasi
     //Utilizzo .split per dividere le lettere della parola e creare un array dove ad ogni carattere corrisponde un indice, partendo da 0.
     //Utilizzato senza spazi tra gli apici divide ogni carattere dal successivo
-    let dividiParola = parola.split("");
+    let dividiParola = minuscolo.replace(/ /g, '').split("");
     console.log(dividiParola);
 
     //Inverto la parola con .reverse, in questo modo inverto l'ordine dei caratteri nell'array
     let invertiParola = dividiParola.reverse();
     console.log(invertiParola);
 
+    /* Questa funziona serve solo per confrontare le parole, non le frasi:
     //Riunisco i caratteri invertiti in una stringa con .join, lascio gli apici senza spazi per non avere distanza tra i caratteri uniti
     let unisciParola = invertiParola.join("");
     console.log(unisciParola);
+    */
 
-    //Confronto la parola invertita con la parola iniziale impostata dall'utente, se combaciano allora è palindroma
-    if (parola == unisciParola) {
-        frase = "La parola è palindroma";
+    //Confronto la sequenza di caratteri invertita con la sequenza di caratteri iniziale impostata dall'utente, se combaciano allora la frase/parola è palindroma
+    if (dividiParola == invertiParola) {
+        frase = "La frase/parola è palindroma";
     } else {
-        frase = "La parola non è palindroma";
+        frase = "La frase/parola non è palindroma";
     }
 
     return frase;
